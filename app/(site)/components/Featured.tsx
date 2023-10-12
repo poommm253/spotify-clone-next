cbf2a7b6120719eaddfe5cc81d8f560702028ba5
Poom C <poommm253@gmail.com>
2023-10-12 01:40:40 -0700

    Implemented Featured Image and Recently Played section

---
+++ app/(site)/components/Featured.tsx
@@ -0,0 +1,5 @@
+export default function Featured() {
+  return (
+    <div className="mx-8 p-5 rounded-lg bg-emerald-100 flex h-[350px]"></div>
+  );
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,16 @@
-import Header from "@/components/Header";
+import Header from "@/app/(site)/components/Header";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import Featured from "./components/Featured";
+import RecentlyPlayed from "./components/RecentlyPlayed";
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-b from-emerald-800 h-1/2">
       <Header />
+      <Featured />
+      <h2 className="py-5 mx-8 text-3xl font-bold">Good Evening</h2>
+      <RecentlyPlayed />
+      <h2 className="pt-10 pb-5 mx-8 text-3xl font-bold">Made For You</h2>
     </div>
   );
 }
