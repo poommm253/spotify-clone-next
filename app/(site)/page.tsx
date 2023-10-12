7b1512534489f338f1bf53a6ef80c8717ed0ffc6
Poom C <poommm253@gmail.com>
2023-10-11 17:56:32 -0700

    implemented main content background gradient and header

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-b from-emerald-800 h-1/2">
+      <Header />
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { GoBell, GoPeople } from "react-icons/go";
+import { MdAccountCircle } from "react-icons/md";
+
+export default function Header() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-row justify-between w-full">
+      <div className="flex flex-row p-5 space-x-2">
+        <button
+          className="items-center justify-center transition bg-black rounded-full hover:opacity-75"
+          onClick={() => router.back()}
+        >
+          <RxCaretLeft size={35}></RxCaretLeft>
+        </button>
+
+        <button
+          className="items-center justify-center hidden transition bg-black rounded-full md:flex hover:opacity-75"
+          onClick={() => router.forward()}
+        >
+          <RxCaretRight size={35}></RxCaretRight>
+        </button>
+      </div>
+      <div className="flex flex-row p-5 space-x-2">
+        <button className="items-center justify-center transition bg-black rounded-full hover:opacity-75">
+          <GoBell className="p-2" size={35}></GoBell>
+        </button>
+        <button className="items-center justify-center transition bg-black rounded-full hover:opacity-75">
+          <GoPeople className="p-2" size={35}></GoPeople>
+        </button>
+
+        <button className="items-center justify-center transition bg-black rounded-full hover:opacity-75">
+          <MdAccountCircle className="p-1" size={35}></MdAccountCircle>
+        </button>
+      </div>
+    </div>
+  );
+}
